c7f49bac8b7e4440354c5e172d844d7902c6be8f
Dũng Trần <tad88.dev@gmail.com>
2017-09-08 10:37:29 +0700

    Add xorcrypt

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# xorcrypt
-Simple XOR encryption
+# xorCrypt
+
+Simple and lightweight algorithm, this encryption use `SHA256` as `PRNG` to create XOR-KEY.
+
+## Features
+- XOR-KEY will be change every 32 bytes
+- XOR-KEY was created from `SHA256` of `SecrectKey` and `OpenSSL RNG's value`
+- Simple
+- Lightweight
+- Easy to implement
+- Data don't repeate by sequence
+- Hash based
+
+## Installation
+Require NodeJs
+```bash
+npm install xorcrypt
+```
+## Usage
+```javascript
+const xorCrypt = require('xorcrypt');
+
+let xo = new xorCrypt(new Buffer('My secret key, I will share with my friend.'));
+let v, e, d;
+v = new Buffer('This is my secret message!');
+
+e = xo.encrypt(v);
+d = xo.decrypt(e);
+console.log(`Value: ${v}\nEncrypted: ${e.toString('hex')}\nDecrypted: ${d.toString()}`);
+```
+Result:
+```bash
+chiro@moonfang:~/labs$ node test.js 
+Value: This is my secret message!
+Encrypted: cc8a630f18837251bd1bbda0c247c443d3cccfe2bfdf598839836f74a0eff248b45fba8b4c445726be3c
+Decrypt: This is my secret message!
+chiro@moonfang:~/labs$ node test.js 
+Value: This is my secret message!
+Encrypted: 0bddba0319d71411503cb129d95eaa2006e33f9e5a3fdacce716d3e4b16e79cdeca63c1f3abc589b2af5
+Decrypted: This is my secret message!
+```
+## License
+This module distributed under [MIT License](https://github.com/tad88dev/xorcrypt/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,90 @@
+const crypto = require('crypto');
+const XOR_KEY_SIZE = 16;
+
+/**
+ * Create bigger xor key from static key and random bytes
+ * @param {Buffer} key 
+ * @param {Buffer} rnd 
+ */
+function _xorKey(key, rnd) {
+    let rawKey = new Buffer(key.length + rnd.length);
+    key.copy(rawKey, 0, 0);
+    rnd.copy(rawKey, key.length, 0);
+    return crypto.createHash('sha256').update(rawKey).digest();
+}
+
+/**
+ * Xor data with key
+ * @param {Buffer} data 
+ * @param {Buffer} key 
+ */
+function _xor(data, key) {
+    let otk = crypto.createHash('sha256').update(key).digest();
+    let buf = new Buffer(data.length);
+    for (let i = 0, c = 0; i < data.length; i++ , c++) {
+        if (c == key.length) {
+            otk = crypto.createHash('sha256').update(key).update(otk).digest();
+            c = 0;
+        }
+        buf[i] = data[i] ^ otk[c];
+    }
+    return buf;
+}
+
+/**
+ * Xor Crypt constructor
+ * @param {Buffer} key 
+ */
+function xorCrypt(key) {
+    let _self = this;
+
+    if (typeof (key) === 'undefined') {
+        var key = new Buffer('You will not know about me');
+    } else if (!Buffer.isBuffer(key)) {
+        throw new TypeError("Key have to a Buffer instance");
+    }
+    this._key = _xorKey(key, new Buffer('48656c6c6f2c49276d20436869726f21', 'hex'));
+    this.encrypt = (data) => {
+        if (!Buffer.isBuffer(data)) {
+            throw new TypeError("Data have to a Buffer instance");
+        }
+        return this._encrypt(data, _self._key, _self._random());
+    }
+    this.decrypt = (data) => {
+        if (!Buffer.isBuffer(data)) {
+            throw new TypeError("Data have to a Buffer instance");
+        }
+        return this._decrypt(data, _self._key);
+    }
+}
+
+/**
+ * Random bytes from OpenSSL
+ */
+xorCrypt.prototype._random = () => {
+    return crypto.randomBytes(XOR_KEY_SIZE);
+}
+
+/**
+ * Encrypt method
+ */
+xorCrypt.prototype._encrypt = (data, key, rnd) => {
+    let buf = new Buffer(data.length + rnd.length);
+    let encrypted = _xor(data, _xorKey(key, rnd));
+    rnd.copy(buf, 0, 0);
+    encrypted.copy(buf, rnd.length, 0);
+    return buf;
+}
+
+/**
+ * Decrypt method
+ */
+xorCrypt.prototype._decrypt = (data, key) => {
+    let buf = new Buffer(data.length - XOR_KEY_SIZE);
+    let rnd = new Buffer(XOR_KEY_SIZE);
+    data.copy(rnd, 0, 0, rnd.length);
+    data.copy(buf, 0, rnd.length);
+    return _xor(buf, _xorKey(key, rnd));
+}
+
+module.exports = xorCrypt;
