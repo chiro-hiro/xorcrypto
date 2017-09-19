e5bd5a7f1998aeb137ff1d92f932ef48c40ea069
Dũng Trần <tad88.dev@gmail.com>
2017-09-19 21:18:46 +0700

    Update mocha testsuite

--- test/test.js
+++ test/test.js
@@ -1,34 +1,45 @@
-const test = require('tape');
 const crypto = require('crypto');
+const assert = require('assert');
 const xorCrypto = require('../index.js');
 
-test('Randomize test', function (t) {
-    let myCrypto = new xorCrypto(crypto.randomBytes(1024));
-    for (let i = 0; i < 10; i++) {
-        let data = crypto.randomBytes(10240);
+describe('Randomize test', function () {
+    it('Randomize test with 10240 bytes from randomBytes()', function () {
+        let myCrypto = new xorCrypto(crypto.randomBytes(1024));
+        for (let i = 0; i < 10; i++) {
+            let data = crypto.randomBytes(10240);
+            let encrypted = myCrypto.encrypt(data);
+            let decrypted = myCrypto.decrypt(encrypted);
+            assert.deepEqual(data, decrypted);
+        }
+    });
+})
+
+describe('Test null bytes key', function (t) {
+    it('Test empty key', function () {
+        let myCrypto = new xorCrypto(Buffer.alloc(1024));
+        let data = crypto.randomBytes(1024);
         let encrypted = myCrypto.encrypt(data);
-        let decrypted = myCrypto.decrypt(encrypted);
-        t.deepEqual(data, decrypted);
-    }
-    t.end();
+
+        assert.equal(typeof (myCrypto), 'object');
+        assert.deepEqual(data, myCrypto.decrypt(encrypted));
+    });
+    it('Test empty key', function () {
+        let myCrypto = new xorCrypto();
+        let data = crypto.randomBytes(1024);
+        let encrypted = myCrypto.encrypt(data);
+        assert.equal(typeof (myCrypto), 'object');
+        assert.deepEqual(data, myCrypto.decrypt(encrypted));
+    });
+
 })
 
-test('Test null key', function (t) {
-    let myCrypto = new xorCrypto(Buffer.alloc(1024));
-    let data = crypto.randomBytes(1024);
-    let encrypted = myCrypto.encrypt(data);
+describe('Decrypt without encrypt', function (t) {
+    it('Descrypt from random bytes', function () {
+        let myCrypto = new xorCrypto(crypto.randomBytes(1024));
+        let data = crypto.randomBytes(1024);
+        let decrypted = myCrypto.decrypt(data);
 
-    t.equal(typeof (myCrypto), 'object');
-    t.deepEqual(data, myCrypto.decrypt(encrypted));
-    t.end();
-})
-
-test('Decrypt without encrypt', function (t) {
-    let myCrypto = new xorCrypto(crypto.randomBytes(1024));
-    let data = crypto.randomBytes(1024);
-    let decrypted = myCrypto.decrypt(data);
-
-    t.equal(data.length - decrypted.length, 16);
-    t.deepEqual(myCrypto.decrypt(data), decrypted);
-    t.end();
+        assert.equal(data.length - decrypted.length, 16);
+        assert.deepEqual(myCrypto.decrypt(data), decrypted);
+    });
 })
