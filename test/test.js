cf2fcb0222138e78b624eb42b4d77f754e099eeb
Chiro Hiro <chiro@fkguru.com>
2019-05-14 16:31:03 +0700

    Update mocha to prevent vulnerable

--- test/test.js
+++ test/test.js
@@ -3,7 +3,8 @@ const assert = require('assert');
 const xorCrypto = require('../index.js');
 
 describe('Randomize test', function () {
-  for (let i = 1; i < 100; i++) {
+
+  for (let i = 1; i < 25; i++)
     it(`Randomize test with ${10240 * i} bytes from randomBytes()`, function () {
       let myCrypto = new xorCrypto(crypto.randomBytes(1024));
       let data = crypto.randomBytes(10240 * i);
@@ -11,5 +12,14 @@ describe('Randomize test', function () {
       let decrypted = myCrypto.decrypt(encrypted);
       assert.deepEqual(data, decrypted);
     });
-  }
+
+  for (let i = 1; i < 25; i++)
+    it(`Zero buffer test with ${10240 * i} bytes`, function () {
+      let myCrypto = new xorCrypto(crypto.randomBytes(1024));
+      let data = Buffer.alloc(10240 * i);
+      let encrypted = myCrypto.encrypt(data);
+      let decrypted = myCrypto.decrypt(encrypted);
+      assert.deepEqual(data, decrypted);
+    });
+
 })
